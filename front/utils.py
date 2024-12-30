66848de727ca76c63ffc2dffcd5c5dfba1ff658d
kCMI113 <cmkang1316@gmail.com>
2024-12-31 08:39:13 +0900

    feat: add func to show items

--- front/utils.py
+++ front/utils.py
@@ -20,6 +20,18 @@ def show_img(
     )
 
 
+@st.cache_resource
+def show_item(item_info):
+    show_img(item_info["item_title"], item_info["img_url"])
+    _, temp, _ = st.columns([0.37, 0.4, 0.23])
+    with temp:
+        with st.popover("info"):
+            st.markdown(f"출연진 : {item_info['people']}")
+            st.markdown(f"개봉일 : {item_info['release']}")
+            st.markdown(f"평균 평점 : {item_info['avg_score']}")
+            st.markdown(f"[더보기]({item_info['item_info']})")
+
+
 def init_session_state():
     st.session_state.user_id = ""
     st.session_state.rec_results = None
