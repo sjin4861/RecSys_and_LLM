a4a8863db4ee4670032d254945a5fc56dd16c37b
kCMI113 <cmkang1316@gmail.com>
2024-12-31 08:38:15 +0900

    feat: add func to show img

--- front/utils.py
+++ front/utils.py
@@ -1,4 +1,23 @@
+from io import BytesIO
+
+import requests
 import streamlit as st
+from PIL import Image
+
+
+@st.cache_resource
+def show_img(
+    item_title: str = "title",
+    img_url: str = "https://ghcci.korcham.net/images/no-image01.gif",
+):
+    image = Image.open(BytesIO(requests.get(img_url, stream=True).content)).resize(
+        (600, 900)
+    )
+    st.image(
+        image,
+        caption=item_title,
+        use_container_width=True,
+    )
 
 
 def init_session_state():
