0b1763355e6a31f7a84eb0cd6a206a20d5eea12e
Yerin99 <diluny2@naver.com>
2025-01-08 21:54:23 +0900

    feat: update extract_title.py to remove 'VHS' from titles

--- crawling/extract_title.py
+++ crawling/extract_title.py
@@ -12,10 +12,20 @@ items = range(1, 86678 + 1)
 
 for i in items:
     title = text_name_dict[t].get(i, t_)
+    # title split 하기
+    split_title = title.split()
+
+    # 마지막 단어에 "VHS"가 있으면 제거
+    if split_title and "VHS" in split_title[-1]:
+        split_title.pop()
+
+    # 원상 복구
+    title = " ".join(split_title)
+
     titles.append(title)
 
 # 추출한 title을 txt 파일에 저장
-with open("titles_only.txt", "w", encoding="utf-8") as f:
+with open("titles_without_vhs.txt", "w", encoding="utf-8") as f:
     for title in titles:
         f.write(f"{title}\n")
 
