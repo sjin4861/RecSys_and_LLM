92102d87b1376315410c7e2e6148aeeb7f3368a9
kCMI113 <cmkang1316@gmail.com>
2025-01-13 19:25:42 +0900

    feat: add clickable image linking to info url

--- front/utils.py
+++ front/utils.py
@@ -1,18 +1,29 @@
+import base64
 from io import BytesIO
 
 import requests
 import streamlit as st
 from PIL import Image
+from st_clickable_images import clickable_images
 
 
 @st.cache_resource
 def show_img(
     item_title: str = "title",
     img_url: str = "https://ghcci.korcham.net/images/no-image01.gif",
+    info_url: str = None,
 ):
     image = Image.open(BytesIO(requests.get(img_url, stream=True).content)).resize(
         (600, 900)
     )
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    html = (
+        f"<a href='{info_url}'><img srcimg src='data:image/png;base64,{img_str}'></a>"
+    )
+    st.markdown(html, unsafe_allow_html=True)
     st.image(
         image,
         caption=item_title,
