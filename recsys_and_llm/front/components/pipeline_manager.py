61e113581dbdfe7448400fcb2a4c12f9a5df2c47
sjin4861 <sjin4861@pusan.ac.kr>
2025-01-29 13:00:53 +0900

    feat : recwizard의 파이프라인 형성 모듈 구현

--- recsys_and_llm/front/components/pipeline_manager.py
+++ recsys_and_llm/front/components/pipeline_manager.py
@@ -1,14 +1,55 @@
 # app/components/pipeline_manager.py
+import os
+
+from dotenv import load_dotenv
+from recwizard import (
+    ChatgptGen,
+    ExpansionConfig,
+    ExpansionPipeline,
+    FillBlankConfig,
+    FillBlankPipeline,
+    RedialRec,
+    UnicrsGen,
+    UnicrsRec,
+)
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+##################
+# Recommendation #
+##################
+unicrs_rec = UnicrsRec.from_pretrained("recwizard/unicrs-rec-redial")
+##################
+#   Generation   #
+##################
+unicrs_gen = UnicrsGen.from_pretrained("recwizard/unicrs-gen-redial")
+gpt_gen = ChatgptGen.from_pretrained("RecWizard/chatgpt-gen-fillblank")
+##################
+# Configurations #
+##################
+fill_blank_config = FillBlankConfig()
+expansion_config = ExpansionConfig()
 
 
-def set_pipeline(flag, rec_module, gen_module):
+def set_pipeline(flag, rec_module=unicrs_rec, gen_module=gpt_gen):
     """
     flag, rec_module, gen_module에 따라 적절한 Pipeline 객체 생성.
     """
     if flag.lower() == "blank":
-        return "FillBlankPipeline"
+        return FillBlankPipeline(
+            config=FillBlankConfig(), rec_module=rec_module, gen_module=gen_module
+        )
     elif flag.lower() == "expansion":
-        return "ExpansionPipeline"
+        return ExpansionPipeline(
+            config=ExpansionConfig(), rec_module=rec_module, gen_module=gen_module
+        )
     elif flag.lower() == "gpt":
-        return "GptPipeline"
+        return FillBlankPipeline(
+            config=FillBlankConfig(), rec_module=rec_module, gen_module=gen_module
+        )
+    elif flag.lower() == "default":
+        return FillBlankPipeline(
+            config=FillBlankConfig(), rec_module=unicrs_rec, gen_module=unicrs_gen
+        )
     return "DefaultUniCRS"
