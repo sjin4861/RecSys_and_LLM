8ee0d11121f30ada4fd36479ddc437a7a6e8f900
kCMI113 <cmkang1316@gmail.com>
2025-02-14 12:33:15 +0900

    feat: change the linked website to item_page

--- recsys_and_llm/front/utils/image_utils.py
+++ recsys_and_llm/front/utils/image_utils.py
@@ -1,5 +1,6 @@
 # utils/image_utils.py
 import base64
+import os
 from io import BytesIO
 
 import requests
@@ -7,10 +8,10 @@ import streamlit as st
 from PIL import Image
 
 
+@st.cache_data
 def show_img(
-    item_title: str = "title",
+    item_id: str,
     img_url: str = "https://ghcci.korcham.net/images/no-image01.gif",
-    info_url: str = None,
 ):
     """
     이미지 URL을 통해 이미지를 로드하고 Streamlit에 표시
@@ -22,5 +23,5 @@ def show_img(
     image.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-    html = f"<a href='{info_url}'><img src='data:image/png;base64,{img_str}'></a>"
+    html = f"<a href='{os.environ.get('BASE_URL')}/item_page/?user={st.session_state.user_id}&item={item_id}' target = '_self'><img src='data:image/png;base64,{img_str}'></a>"
     st.markdown(html, unsafe_allow_html=True)
