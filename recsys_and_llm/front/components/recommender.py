752399c2a3789494ffef76e5822fbe42a14a6a2f
kCMI113 <cmkang1316@gmail.com>
2025-03-07 03:45:24 +0000

    chore: change css container name and header ment

--- recsys_and_llm/front/components/recommender.py
+++ recsys_and_llm/front/components/recommender.py
@@ -11,36 +11,49 @@ def rec_main(item: dict):
         st.warning("추천 결과가 없습니다.")
         return
 
-    st.header(f"{st.session_state.user_name}님 지금 이 영화 어떠세요?")
+    st.header(f"{st.session_state.user_name}님, 지금 Title 어떠세요?")  # TODO title
 
     # 이미지와 링크 표시
     html = f"""
     <style>
-        .image-container {{
+        .rec-image-container {{
             text-align: center;
             margin-top: 20px;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            max-width: 500px;
+            margin-left: auto;
+            margin-right: auto;
         }}
-        .image-container img {{
-            width: 400px;
-            height: 600px;
+        
+        .rec-image-container img {{
+            width: 100%;
+            height: auto;
             border-radius: 16px;
             box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2), 0 4px 8px rgba(0, 0, 0, 0.1);
             transition: transform 0.3s ease, box-shadow 0.3s ease;
+            object-fit: cover;
         }}
-        .image-container img:hover {{
+
+        .rec-image-container img:hover {{
             transform: scale(1.05);
             box-shadow: 0 12px 24px rgba(0, 0, 0, 0.3);
         }}
-        .image-container p {{
+
+        .rec-image-container p {{
             margin-top: 10px;
             font-size: 18px;
             color: #444;
             font-weight: bold;
         }}
     </style>
-    <div class="image-container">
-        <a href={os.environ.get("FRONT_URL")}/item_page/?user={st.session_state.reviewer_id}&name={st.session_state.user_name}&item={item["item_id"]} target = '_self'>
-            <img src="{item['img_url']}">
+    
+    <div class="rec-image-container">
+        <a href="{os.environ.get('FRONT_URL')}/item_page/?user={st.session_state.reviewer_id}&name={st.session_state.user_name}&item={item['item_id']}" target="_self">
+            <img src="{item['img_url']}" alt="Item Image">
         </a>
     </div>
     """
